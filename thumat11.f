afebaa6a20d2c323c9f699c0c7636a5aa9a1e18c
jfriedlein <Johannes.Friedlein@fau.de>
2022-10-25 12:03:17 +0200

    initial commit

---
+++ thumat11.f
@@ -0,0 +1,69 @@
+     subroutine thumat11(c1,c2,c3,cvl,dcvdtl,hsrcl,dhsrcdtl,
+     1 hsv,hsvm,nmecon,r_matp,crv,nnpcrv,
+     2 nel,nep,iep,eltype,dt,atime,ihsrcl,temp,ttconv,ttnext,
+     3 tmconv,tmnext,inithis)
+c
+c******************************************************************
+c|  Livermore Software Technology Corporation  (LSTC)             |
+c|  ------------------------------------------------------------  |
+c|  Copyright 1987-2008 Livermore Software Tech. Corp             |
+c|  All rights reserved                                           |
+c******************************************************************
+c
+c For this thermal user material to properly work, the following two cards need to be used (in addition to others):
+c *CONTROL_THERMAL_SOLVER
+c $ eqheat&fwork: The generation of heat (temperature) from plastic deformation as eqheat and fwork are deactived by setting them to "1e-60" (numerically 0). Note that entering "0" would render the default value of "1", which does not deactive this.
+c $#   atype     ptype    solver     cgtol       gpt    eqheat     fwork       sbc
+c          1         1         11.00000E-4         0     1e-60     1e-60       0.0
+c $#  msglvl    maxitr    abstol    reltol     omega    unused    unused       tsf
+c          0       5001.0000E-101.00000E-4       1.0                           1.0
+c $#   mxdmp      dtvf    varden      
+c          0       0.0         0
+c *CONTROL_THERMAL_TIMESTEP
+c $ tip: use "1.0" to avoid intermediate steps, which would require a reduced source term
+c $ its: the initial thermal time step size (its) is here set to "0.1". Note that you need to set this according to your simulation model (ideally equal to mechanical time step size).
+c $ dtemp: use "1e6" to make sure that the thermal step size is never reduced, which would require a reduced source term; a reduced thermal step is not helpful to increase the accuracy, but would only be necessary in case of convergence failure
+c $#      ts       tip       its      tmin      tmax     dtemp      tscp      lcts
+c          0       1.0       0.1       0.0         1       1e6       0.5         0
+c
+c******************************************************************
+c
+       include 'nlqparm'
+       include 'iounits.inc'
+c
+       character*(*) eltype
+       logical inithis
+       dimension hsv(*),hsvm(*),r_matp(*),crv(lq1,2,*)
+       integer nnpcrv(*)
+       real*8, intent(out) :: c1,c2,c3,cvl
+       real*8, intent(in) :: temp
+       real*8, intent(out) :: hsrcl       
+c       
+       real*8 L, d_loc
+c
+c Internal length as parameter 1 of thermal material card
+       L = r_matp(8+ 1 )
+c       
+c Retrieve truly local damage (entry 1, see umat41) from mechanical subroutine history, herein named "hsvm"
+c @note Requires IHVE=1
+       d_loc = hsvm(7+ 1 )
+c
+c Thermal Conductivity (parameter in front of Laplacian of temperature)
+       c1 = L**2
+c
+c Other parameters of thermal conductivity are zero
+       c2=0.
+       c3=0.
+       cvl=0.
+c       
+c Activate the source term
+       ihsrcl = 1
+c       
+c Enter the source term as difference between temperature and truly local damage
+       hsrcl = -temp + d_loc
+c      
+c Derivative of source term hsrcl with respect to temperature temp
+        dhsrcdtl = -1.
+c
+       return
+       end
---
+++ umat41.f
@@ -0,0 +1,51 @@
+      subroutine umat41 (cm,eps,sig,epsp,hsv,dt1,capa,etype,tt,
+     1 temper,failel,crv,nnpcrv,cma,qmat,elsiz,idele,reject)
+c
+c ...
+        real*8 d_loc_n1, d_loc_n
+        real*8 d_n1, d_n
+        real*8 d_NL_n1, d_NL_n
+        real*8 K_NL
+c ...
+c Compute the truly local damage "d_loc_n1" with whatever damage law you like, as before
+        d_loc_n1 = ...
+c
+c The temperature "temper" contains the old non-local damage variable
+        d_NL_n = temper
+c Retrieve the old truly local damage "d_loc_n" from the history "hsv"
+        d_loc_n = hsv(1)
+c Compute the non-local factor "K_NL"
+        K_NL = d_NL_n / d_loc_n
+c To avoid issues with division by zero and large undesired fluctuations
+c  the non-local factor K_NL is possibly corrected as follows:
+        ! if old non-local variable is zero, we have to use the truly local
+        !  else e.g. the damage would always be delayed
+         if ( d_NL_n < 1e-14 ) then
+           K_NL = 1.
+        ! if old truly local variable is zero, we have to use the truly local
+         elseif ( d_loc_n < 1e-14 ) then
+           K_NL = 1.
+        ! If damage shall be spread (K_NL>1), better do not scale the truly local variable to avoid extremely large values. We rely on the below "max(max(..,phi_n1),...)" for spreading.
+         elseif ( K_NL > 1. ) then
+           K_NL = 1.
+         endif
+c Update the non-local damage "d_NL_n1"
+        d_NL_n1 = K_NL * d_loc_n1
+c Retrieve the old local damage variable "d_n" from the history "hsv"
+        d_n = hsv(2)
+c Compute the new local damage "d_n1"
+        d_n1 = max( max( d_n, d_NL_n ), d_NL_n1 )
+c
+c Use the new local damage "d_n1" to damage the material behaviour, as before
+c e.g. stress = (1-d_n1) * stress
+c ...
+c
+c Store as history variable "hsv":
+c  the truly local damage "d_loc_n1"
+        hsv(1) = d_loc_n1
+c  the new local damage variable "d_n1"
+        hsv(2) = d_n1
+c
+      return
+      end
+
